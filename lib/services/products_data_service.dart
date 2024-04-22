56f9d8072a138c856a3cf9fb483739c0d5edd7ee
Gino <gino.laranjeiras@onikel.com>
2024-04-22 16:49:06 -0300

    Adicionando serviço de dados de produtos ao projeto

---
+++ lib/services/products_data_service.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../common_libs.dart';
+
+class ApiService {
+  String argument = '*';
+  String original = 'true';
+  String path =
+      'https://gateway-smartforce-dev.devyandeh.com.br/process/candidates/search-products';
+
+  Future<Catalog> fetchCatalog() async {
+    Map<String, String> headers = {
+      'token': '3855a045-db2b-4729-84e1-6398f2fbdeb7',
+      'Accept': '*/*',
+      'Host': 'gateway-smartforce-dev.devyandeh.com.br',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+    };
+
+    final response = await http.get(
+      Uri.parse('$path?argument=$argument&original=$original'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return _handleSuccess(response);
+    } else if (response.statusCode == 404) {
+      return _handleNotFound(response);
+    } else if (response.statusCode >= 500) {
+      return _handleServerError(response);
+    } else {
+      return _handleOtherResponses(response);
+    }
+  }
+
+  Catalog _handleSuccess(http.Response response) {
+    Map<String, dynamic> json = jsonDecode(response.body);
+    return Catalog.fromJson(json);
+  }
+
+  Catalog _handleNotFound(http.Response response) {
+    throw Exception('Not found: ${response.body}');
+  }
+
+  Catalog _handleServerError(http.Response response) {
+    throw Exception('Server error: ${response.body}');
+  }
+
+  Catalog _handleOtherResponses(http.Response response) {
+    throw Exception('Unexpected response: ${response.body}');
+  }
+}
