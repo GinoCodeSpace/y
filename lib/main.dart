e75b235fffe21d6c98d42add670ebf415ccdb339
Gino <gino.laranjeiras@onikel.com>
2024-04-22 17:05:29 -0300

    Corrigindo importação do pacote flutter_dotenv em main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'common_libs.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const YApp());
 }
