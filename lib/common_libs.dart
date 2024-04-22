da1a18bd8db898738e02766d7278717e6fbd65b3
Gino <gino.laranjeiras@onikel.com>
2024-04-22 16:45:24 -0300

    Adicionando arquivos comuns ao projeto

---
+++ lib/common_libs.dart
@@ -0,0 +1,61 @@
+// packages
+export 'package:flutter/material.dart';
+
+// app
+export 'y_app.dart';
+
+// models
+// product
+export 'models/product.dart';
+
+//section
+export 'models/section.dart';
+
+// catalog
+export 'models/catalog.dart';
+
+// ui
+// styles
+export 'ui/styles/themes/y_light_theme.dart';
+
+// utils
+// ui constants
+export 'ui/styles/utils/ui_constants.dart';
+
+// layout utils
+export 'ui/styles/utils/layout_utils.dart';
+
+// pages
+// dashboard page
+export 'ui/pages/dashboard_page.dart';
+
+// components
+// custom sliver app bar
+export 'ui/components/custom_sliver_mobile_app_bar.dart';
+
+// custom flexible space bar mobile
+export 'ui/components/custom_flexible_space_bar_mobile.dart';
+
+// custom flexible space bar desktop
+export 'ui/components/custom_flexible_space_bar_desktop.dart';
+
+// cards carousel
+export 'ui/components/hero_carousel_card.dart';
+
+// hero section
+export 'ui/components/hero_section.dart';
+
+// hero carousel
+export 'ui/components/hero_carousel.dart';
+
+// promotion section
+export 'ui/components/promotion_section.dart';
+
+// product section
+export 'ui/components/product_section.dart';
+
+// product card
+export 'ui/components/product_card.dart';
+
+// custom sections
+export 'ui/components/custom_sections.dart';
